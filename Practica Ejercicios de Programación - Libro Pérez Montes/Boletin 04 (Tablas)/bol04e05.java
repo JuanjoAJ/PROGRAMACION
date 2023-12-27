4164fd3e3e564d265ff975093ea30124d2c0c9c7
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2023-12-27 19:14:39 +0100

    up

---
+++ Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 04 (Tablas)/bol04e05.java
@@ -0,0 +1,8 @@
+/*Leer por teclado dos tablas de 10 números enteros y mezclarlas en una tercera de la forma: el 1º de A, el 1º
+de B, el 2º de A, el 2º de B, etc.
+*/
+public class bol04e05 {
+    public static void main(String[] args) {
+
+    }
+}
