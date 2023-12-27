3b3deeeb76309ef78920a15610a05cd028702256
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2023-12-27 21:14:19 +0100

    up

---
+++ Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 04 (Tablas)/bol04e08.java
@@ -0,0 +1,9 @@
+/* Diseñar una aplicación que declare una tabla de 10 elementos enteros. Leer mediante el teclado 8 números.
+Después se debe pedir un número y una posición, insertarlo en la posición indicada, desplazando los que estén
+detrás.
+*/
+public class bol04e08 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 04 (Tablas)/bol04e09.java
@@ -0,0 +1,8 @@
+/*Crear un programa que lea por teclado una tabla de 10 números enteros y la desplace una posición hacia
+abajo: el primero pasa a ser el segundo, el segundo pasa a ser el tercero y así sucesivamente. El último pasa a
+ser el primero.*/
+public class bol04e09 {
+    public static void main(String[] args) {
+
+    }
+}
