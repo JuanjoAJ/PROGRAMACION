dd2ae81be4fa26bdfc3110c499430b00219219e1
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2023-12-28 19:27:05 +0100

    up

--- Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 04 (Tablas)/bol04e12.java
+++ Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 04 (Tablas)/bol04e12.java
@@ -5,6 +5,19 @@ elemento situado en la posición dada sin dejar huecos.*/
 public class bol04e12 {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
+        int[] array=new int[10];
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Introduce número");
+            array[i]=s.nextInt();
+        }
+        System.out.println("Introduce posición a eliminar");
+        int n=s.nextInt();
+
+        for (int i = n; i < array.length-1; i++) {
+            array[i]=array[1+i];
+        }array[array.length-1]=0;
+
+        for (int item:array) System.out.print(item + " ");
 
     }
 }
--- Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 04 (Tablas)/bol04e16.java
+++ Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 04 (Tablas)/bol04e16.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 /* Queremos desarrollar una aplicación que nos ayude a gestionar las notas de un centro educativo. Cada
 grupo (o clase) está compuesto por 5 alumnos. Se pide leer las notas del primer, segundo y tercer
 trimestre de un grupo. Debemos mostrar al final: la nota media del grupo en cada trimestre, y la media
 del alumno que se encuentra en la posición N (N se lee por teclado).
 */
 public class bol04e16 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+
+
+
+    }
 }
