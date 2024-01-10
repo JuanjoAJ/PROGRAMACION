4761a70b383ae567fe26e23714099bf99039d4f4
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2024-01-10 20:12:24 +0100

    up

--- Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 06 (Funciones)/bol06e34.java
+++ Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 06 (Funciones)/bol06e34.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 /* Igual que el ejercicio anterior, pero pudiendo configurar los valores de los dos primeros término
 de la serie.
 */
 public class bol06e34 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int num, fibUno, fibDos;
+
+        System.out.println("Introduce el primer valor de fibonacci");
+
+        System.out.println("Introduce el segundo valor de fibonacci");
+        System.out.println("Introduce número");
+
+
+    }
 }
---
+++ Practica Ejercicios de Programación - Libro Pérez Montes/Boletin 06 (Funciones)/bol06e36.java
@@ -0,0 +1,5 @@
+/*DUF que toma una tabla bidimensional de enteros, representando un tablero de ajedrez. Disponemos
+de las constantes PB (peón blanco), TN (torre negra), etc. (P, T, C, A, R, D). Dicho módulo debe
+devolver un valor booleano, que indique si el rey negro está amenazado.*/
+public class bol06e36 {
+}
