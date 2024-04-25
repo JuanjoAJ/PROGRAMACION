0057b3fd51dd6217ba47d49925203872f7c6f891
Juan José Ávila <148000889+JuanjoAJ@users.noreply.github.com>
2024-04-25 10:52:56 +0200

    JOption

--- T8 - Ficheros/DatosBásicos/MainDatosBasicos.java
+++ T8 - Ficheros/DatosBásicos/MainDatosBasicos.java
@@ -1,6 +1,7 @@
 import controller.ControladorUsuario;
 import model.UsuarioDB;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -15,9 +16,10 @@ public class MainDatosBasicos {
                 System.out.println("Introduce nombre, apellidos, dni, telefono y edad");
 
                 usuarios.addUsuario(scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.nextInt());
-                System.out.println("¿Desea crear a otro usuario?");
+
                 scanner=new Scanner(System.in);
-                if (!scanner.next().equalsIgnoreCase("si")) salir = true;
+                int opcion=JOptionPane.showConfirmDialog(null,"¿Desea crear a otro usuario?", "Creador Usuarios", JOptionPane.YES_NO_OPTION);
+                if (opcion==JOptionPane.NO_OPTION) salir = true;
             }catch (IllegalArgumentException e){
                 System.err.println("Introducción ilegal de datos");
             }
